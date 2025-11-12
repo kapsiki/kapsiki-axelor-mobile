271f60dab6fa75a7c961e7b68cfc54a20440a54e
BAHA Ephraim <jeansamist@gmail.com>
2025-11-12 17:55:57 +0100

    feat: enhance user fetching in HomeScreen and improve favorite folder check in DocumentList

--- packages/kapsiki/src/screens/HomeScreen.tsx
+++ packages/kapsiki/src/screens/HomeScreen.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useCallback, useEffect} from 'react';
+
+import {
+  fetchActiveUser,
+  useDispatch,
+  useSelector,
+} from '@axelor/aos-mobile-core';
+
 import WelcomeHomeView from '../views/welcome-home-view';
-import {TabNavigation} from '../components/tab-navigation';
-import ProjectsHomeView from '../views/projects-home.view';
 
 export default () => {
+  const dispatch = useDispatch();
+  const {userId} = useSelector(state => state.auth);
+
+  const fetchUser = useCallback(() => {
+    dispatch(fetchActiveUser(userId));
+  }, [dispatch, userId]);
+
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
   return <WelcomeHomeView />;
-  // <TabNavigation
-  //   screens={[
-  //     {screen: WelcomeHomeView, title: 'Home', icon: 'house-door'},
-  //     {screen: ProjectsHomeView, title: 'Projects', icon: 'clipboard2-data'},
-  //   ]}
-  // />
 };
